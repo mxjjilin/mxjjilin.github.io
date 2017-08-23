f93062c2f6720b44bb00c9be1437e245578669d6
jilin <mxjjilin@163.com>
2017-08-23 16:56:59 +0800

    dist

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function() {
+  // alert(123)
+  var oTimer = setTimeout(function() {
+    $(".about-me").removeClass('fadeout').addClass('fadein');
+  }, 1000);
+  // $(window).scroll(function() {
+  // var s = $(window).scrollTop();
+  // var top = $("myChart").offset().top - 100;
+  // if (s > top) {
+  $(".container").addClass("active");
+
+
+
+})
